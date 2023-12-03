4571793a854a2a5deb1bbda6ac4e9b4c131795fe
msftnntn <152199184+msftnntn@users.noreply.github.com>
2023-12-04 02:18:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function createLimitedWindows(limit) { //function createInfiniteWindows()
     let x = random(windowWidth);
     let y = random(windowHeight);
     let newWindow = window.open('', '_blank', `width=400,height=200,left=${x},top=${y}`);
-    newWindow.document.body.innerHTML = '<p style="font-size:24px;">당신은 피조물입니까?</p>'; 
+    newWindow.document.body.innerHTML = '<p style="font-size:48px;">당신은 피조물입니까?</p>'; 
 
     windowCount++;
   }, 800); // 0.8 초
